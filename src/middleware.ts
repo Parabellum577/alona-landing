ffa3c64f8b9b3d28fb1495f117d74dc4c93e80c1
Parabellum577 <parabellum577@gmail.com>
2025-02-19 19:14:05 +0100

    locale fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import createMiddleware from 'next-intl/middleware';
 import { defaultLocale } from '@/config/metadata';
 
-// Get the preferred locale, similar to the above or using a different method
 export default createMiddleware({
   // List of all locales that are supported
   locales: ['uk', 'ru'],
@@ -10,7 +9,7 @@ export default createMiddleware({
   defaultLocale,
 
   // Only add locale prefix for non-default locale
-  localePrefix: 'as-needed',
+  localePrefix: 'always',
 
   localeDetection: false
 });
